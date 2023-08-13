b91f4a8792a650bdfaa88c6a93236e34b4264e6f
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-08-13 21:11:21 +0200

    feat(brainfuck): harden error handling

--- brainfuck/src/main.rs
+++ brainfuck/src/main.rs
@@ -3,9 +3,9 @@ use std::io::{stdin, stdout};
 use brainfuck::{parse, tokenize, VirtualMachine};
 
 fn main() {
-    let source = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    let source = "+[>>>->-[>->----<<<]>>]>.---.>+..+++.>>.<.>>---.<<<.+++.------.<-.>>+.";
     let tokens = tokenize(source.chars());
     let code = parse(tokens).unwrap();
     let mut vm = VirtualMachine::new(stdin(), stdout());
-    vm.interpret(&code);
+    vm.interpret(&code).unwrap();
 }
--- brainfuck/src/vm.rs
+++ brainfuck/src/vm.rs
@@ -1,9 +1,16 @@
+use crate::instruction::Instruction;
 use std::io::{Read, Write};
 
-use crate::instruction::Instruction;
+const TAPE_LENGTH: usize = 30_000;
+
+#[derive(Debug)]
+pub enum RuntimeError {
+    InputError,
+    OutputError,
+}
 
 pub struct VirtualMachine<R: Read, W: Write> {
-    pointer: usize,
+    dp: usize, // data pointer
     data: Box<[u8]>,
     read: R,
     write: W,
@@ -12,35 +19,40 @@ pub struct VirtualMachine<R: Read, W: Write> {
 impl<R: Read, W: Write> VirtualMachine<R, W> {
     pub fn new(read: R, write: W) -> Self {
         Self {
-            pointer: 0,
-            data: Box::new([0; 30_000]),
+            dp: 0,
+            data: Box::new([0; TAPE_LENGTH]),
             read,
             write,
         }
     }
 
-    pub fn interpret(&mut self, code: &[Instruction]) {
+    pub fn interpret(&mut self, code: &[Instruction]) -> Result<(), RuntimeError> {
         for instruction in code {
-            use Instruction::*;
+            use {Instruction::*, RuntimeError::*};
             match instruction {
-                Right => self.pointer += 1,
-                Left => self.pointer -= 1,
-                Increment => self.data[self.pointer] += 1,
-                Decrement => self.data[self.pointer] -= 1,
-                Output => {
-                    self.write.write_all(&[self.data[self.pointer]]).unwrap();
-                }
-                Input => {
-                    let mut buffer = [0];
-                    self.read.read_exact(&mut buffer).unwrap();
-                    self.data[self.pointer] = buffer[0];
-                }
+                Right => self.dp = (self.dp + 1) % TAPE_LENGTH,
+                Left => self.dp = self.dp.checked_sub(1).unwrap_or(TAPE_LENGTH - 1),
+                Increment => self.data[self.dp] = self.data[self.dp].wrapping_add(1),
+                Decrement => self.data[self.dp] = self.data[self.dp].wrapping_sub(1),
+                Output => write_u8(&mut self.write, self.data[self.dp]).ok_or(OutputError)?,
+                Input => self.data[self.dp] = read_u8(&mut self.read).ok_or(InputError)?,
                 Loop(body) => {
-                    while self.data[self.pointer] != 0 {
-                        self.interpret(body);
+                    while self.data[self.dp] != 0 {
+                        self.interpret(body)?;
                     }
                 }
             }
         }
+        Ok(())
     }
+}
+
+fn read_u8<R: Read>(read: &mut R) -> Option<u8> {
+    let mut buffer = [0];
+    read.read_exact(&mut buffer).ok()?;
+    Some(buffer[0])
+}
+
+fn write_u8<W: Write>(write: &mut W, value: u8) -> Option<()> {
+    write.write_all(&[value]).ok()
 }
