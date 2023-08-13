0c3fb0bd117f3fda024c3d9b0cdb764c4382bd9f
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-08-13 22:16:55 +0200

    feat(brainfuck): implement command line interface

--- brainfuck/src/lib.rs
+++ brainfuck/src/lib.rs
@@ -3,4 +3,9 @@ mod lexer;
 mod parser;
 mod vm;
 
-pub use {instruction::Instruction, lexer::tokenize, parser::parse, vm::VirtualMachine};
+pub use {
+    instruction::Instruction,
+    lexer::tokenize,
+    parser::{parse, ParseError},
+    vm::{RuntimeError, VirtualMachine},
+};
--- brainfuck/src/main.rs
+++ brainfuck/src/main.rs
@@ -1,11 +1,41 @@
-use std::io::{stdin, stdout};
+use brainfuck::{parse, tokenize, ParseError, RuntimeError, VirtualMachine};
+use clap::Parser;
+use colored::Colorize;
+use std::{
+    fs,
+    io::{stdin, stdout},
+    path::PathBuf,
+    process::ExitCode,
+};
 
-use brainfuck::{parse, tokenize, VirtualMachine};
+fn main() -> ExitCode {
+    match run() {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("{}", err.red());
+            ExitCode::FAILURE
+        }
+    }
+}
 
-fn main() {
-    let source = "+[>>>->-[>->----<<<]>>]>.---.>+..+++.>>.<.>>---.<<<.+++.------.<-.>>+.";
+#[derive(Parser)]
+struct Args {
+    /// Source of the Brainfuck program
+    filename: PathBuf,
+}
+
+fn run() -> Result<(), &'static str> {
+    let args = Args::parse();
+    let source = fs::read_to_string(args.filename)
+        .map_err(|_| "InterpreterError: Could not read file at specified path.")?;
     let tokens = tokenize(source.chars());
-    let code = parse(tokens).unwrap();
+    let code = parse(tokens).map_err(|err| match err {
+        ParseError::UnexpectedLoopEnd => "ParseError: Unexpected loop end (found ']').",
+        ParseError::MissingLoopEnd => "ParseError: Missing loop end (found EOF).",
+    })?;
     let mut vm = VirtualMachine::new(stdin(), stdout());
-    vm.interpret(&code).unwrap();
+    vm.interpret(&code).map_err(|err| match err {
+        RuntimeError::InputError => "RuntimeError: Could not read from input.",
+        RuntimeError::OutputError => "RuntimeError: Could not write to output.",
+    })
 }
