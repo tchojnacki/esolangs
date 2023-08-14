7e1b319348b67330e4719e2867d1a50e9d9eea10
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-08-14 16:02:01 +0200

    feat(brainfuck): add integration tests

---
+++ brainfuck/tests/integration.rs
@@ -0,0 +1,40 @@
+use assert_cmd::Command;
+use std::fs;
+
+fn run_spec(name: &'static str) {
+    const PATH: &str = "tests/spec";
+    let mut command = Command::cargo_bin("brainfuck").unwrap();
+    command.arg(format!("{PATH}/{name}.code.bf"));
+    if let Ok(input) = fs::read_to_string(format!("{PATH}/{name}.in.txt")) {
+        command.write_stdin(input);
+    }
+    let command = command.assert().success();
+    if let Ok(output) = fs::read_to_string(format!("{PATH}/{name}.out.txt")) {
+        command.stdout(output);
+    }
+}
+
+#[test]
+fn add_works() {
+    run_spec("add")
+}
+
+#[test]
+fn even_works() {
+    run_spec("even")
+}
+
+#[test]
+fn fibonacci_works() {
+    run_spec("fibonacci")
+}
+
+#[test]
+fn hello_golf_works() {
+    run_spec("hello-golf")
+}
+
+#[test]
+fn hello_normal_works() {
+    run_spec("hello-normal")
+}
