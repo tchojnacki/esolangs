e4b8ede6e91438888aa4cc8fc7a86162b6627b0b
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-09-23 21:44:09 +0200

    feat(brainfuck): add step to debugger

--- brainfuck/src/bin/bf/debugger.rs
+++ brainfuck/src/bin/bf/debugger.rs
@@ -1,3 +1,5 @@
+use std::num::NonZeroUsize;
+
 use brainfuck::{Instruction, VirtualMachineStd};
 use colored::Colorize;
 use indoc::indoc;
@@ -5,15 +7,22 @@ use rustyline::{error::ReadlineError, DefaultEditor};
 
 use crate::{errors::CliError, source::highlight_source};
 
+enum ReplAction {
+    Resume,
+    Quit,
+    EndSilently,
+}
+
 pub fn run_debugger(mut vm: VirtualMachineStd, source: &str) -> Result<(), String> {
     macro_rules! enter_repl {
         () => {
-            match repl() {
+            match repl(&mut vm, source) {
                 Ok(ReplAction::Resume) => show("Resuming execution..."),
                 Ok(ReplAction::Quit) => {
                     show("Aborting due to a quit from REPL.");
                     return Ok(());
                 },
+                Ok(ReplAction::EndSilently) => return Ok(()),
                 Err(_) => return Err("DebuggerError: Unexpected error in REPL.".to_owned()),
             }
         };
@@ -40,12 +49,7 @@ pub fn run_debugger(mut vm: VirtualMachineStd, source: &str) -> Result<(), Strin
     Ok(())
 }
 
-enum ReplAction {
-    Resume,
-    Quit,
-}
-
-fn repl() -> Result<ReplAction, ReadlineError> {
+fn repl(vm: &mut VirtualMachineStd, source: &str) -> Result<ReplAction, ReadlineError> {
     show("Use :r to resume, use :h to see all commands.");
     let mut rl = DefaultEditor::new()?;
     loop {
@@ -56,29 +60,56 @@ fn repl() -> Result<ReplAction, ReadlineError> {
             Err(other) => return Err(other),
         };
 
-        if let Some(command) = command.strip_prefix(':') {
-            rl.add_history_entry(format!(":{command}"))?;
-            let parts = command.split_whitespace().collect::<Vec<_>>();
-            match parts[0] {
-                "h" | "help" => print_help(),
-                "r" | "resume" => return Ok(ReplAction::Resume),
-                "q" | "quit" => return Ok(ReplAction::Quit),
-                _ => show("Unknown command! Use :h to see all commands."),
-            }
-        } else {
-            show("Executing provided code...");
+        rl.add_history_entry(&command)?;
+        let parts = command.split_whitespace().collect::<Vec<_>>();
+        match *parts.first().unwrap_or(&"") {
+            ":h" | ":help" => exec_help(),
+            ":r" | ":resume" => return Ok(ReplAction::Resume),
+            ":s" | ":step" =>
+                if let Some(action) =
+                    exec_step(vm, source, parts.get(1).copied().unwrap_or("").parse().ok())
+                {
+                    return Ok(action);
+                },
+            ":q" | ":quit" => return Ok(ReplAction::Quit),
+            _ => show("Invalid command! Use :h to see all commands."),
         }
     }
 }
 
-fn print_help() {
+fn exec_help() {
     show(indoc! {"
-        Prompts which do not start with ':' are interpreted as code.
         Available commands:
-            :h, :help      Display the list of available commands
-            :r, :resume    Resume the execution
-            :q, :quit      Abort the execution
+          :h, :help       Display the list of available commands
+          :r, :resume     Resume the execution
+          :s, :step <N>   Execute up to N next instructions [default: 1]
+          :q, :quit       Abort the execution
     "});
+}
+
+fn exec_step(
+    vm: &mut VirtualMachineStd,
+    source: &str,
+    n: Option<NonZeroUsize>,
+) -> Option<ReplAction> {
+    let Some(n) = n else {
+        show("Invalid number of steps!");
+        return None;
+    };
+
+    show(format!("Executing the next {n} instructions...").as_str());
+    for _ in 0..n.into() {
+        match vm.step() {
+            Some(Ok(_)) => (),
+            Some(Err(err)) => {
+                eprintln!("{}", err.message(source).red());
+                break;
+            },
+            None => return Some(ReplAction::EndSilently),
+        }
+    }
+
+    None
 }
 
 fn show(message: impl Colorize) {
